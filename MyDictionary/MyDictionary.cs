e3d47853454a078f7891ad074755e9fe554029fc
semihelibol <semih.elibol@hotmail.com>
2021-01-23 19:04:43 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keyItems;
+        TValue[] valueItems;
+        //constructor
+        public MyDictionary()
+        {
+            keyItems = new TKey[0];
+            valueItems = new TValue[0];
+        }
+        public void Add(TKey keys, TValue values)
+        {
+            TKey[] tempKeys = keyItems;
+            TValue[] tempValues = valueItems;
+           
+            keyItems = new TKey[keyItems.Length + 1];
+            valueItems = new TValue[valueItems.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+               keyItems[i] = tempKeys[i];
+            }
+            keyItems[keyItems.Length - 1] = keys;
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {             
+                valueItems[i] = tempValues[i];
+            }            
+            valueItems[valueItems.Length - 1] = values;           
+        }
+        public int Count
+        {
+            get { return keyItems.Length; }
+        }
+        
+        public TKey[] Keys
+        {
+            get { return keyItems; }
+        }
+
+        public TValue[] Values
+        {
+            get { return valueItems; }
+        }
+    }
+    }
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string > iller = new MyDictionary<int, string>();
+            iller.Add(12, "Bingöl");
+            iller.Add(20, "Denizli");
+            iller.Add(52,"Ordu");
+            
+
+            for (int i = 0; i < iller.Count; i++)
+            {
+                Console.WriteLine(iller.Keys[i]+" : "+iller.Values[i]);
+            }
+
+            Console.WriteLine("Toplam İl Sayısı: " + iller.Count);
+
+        }
+    }
+}
